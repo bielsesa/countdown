bcb35d37cbf2c6999bc8df091b07f1a9a80b9cf9
Biel Serrano Sanchez <bielsesa@gmail.com>
2022-07-13 12:36:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "09/30/",
+      dayMonth = "03/23/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
